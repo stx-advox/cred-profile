327f688b4d93961b5df2a1f920ed2e02a547d0dc
hz <13457698+hozzjss@users.noreply.github.com>
2023-04-07 23:18:56 +0200

    rookie mistache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@cred-profile/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@cred-profile/styles/globals.css";
+import type { AppProps } from "next/app";
+import "@picocss/pico";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
